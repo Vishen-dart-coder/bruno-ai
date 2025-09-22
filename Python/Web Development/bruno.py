98e36de5e08a0610749b5b0b100ea762a4bfe62d
Vishen-dart-coder <iamvishensharma@gmail.com>
2025-09-22 19:46:20 +0530

    Delete Python directory

--- Python/Web Development/bruno.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-from pydantic import BaseModel
-from langchain_openai import ChatOpenAi
-
-load_dotenv()
-
-llm = ChatOpenAi(model="gpt-5")
-responce = llm.invoke("Who are you?")
-print(responce)
